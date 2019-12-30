51cb6968e4359c9868e3826523e06bef2400a4fd
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-30 11:30:51 -0300

    feat: cleanup stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Study Blog`,
+    description: `A blog for Gatsby's study purpose`,
+    author: `@felipemelchior`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
