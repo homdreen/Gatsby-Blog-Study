d243f49672d6f1c306c4ae3aabb49b145bd2b9d1
Felipe Homrich Melchior <fehmel@gmail.com>
2019-12-31 19:02:54 -0300

    feat: add styled-components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Profile from './Profile';
+import Profile from '../Profile';
+
+import { LayoutWrapper, LayoutMain } from './styles';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <LayoutWrapper>
       <aside>
         <Profile />
       </aside>
-      <main>{children}</main>
-    </>
+      <LayoutMain>{children}</LayoutMain>
+    </LayoutWrapper>
   );
 }
 
