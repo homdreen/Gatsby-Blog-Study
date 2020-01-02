f9b7a238bbe2873ff45800793fbe70dbf658d147
Felipe Homrich Melchior <fehmel@gmail.com>
2020-01-02 15:13:37 -0300

    feat: add postItem component

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Container,
+  PostItemLink,
+  PostItemDate,
+  PostItemDescription,
+  PostItemInfo,
+  PostItemTag,
+  PostItemTitle,
+} from './styles';
+
+export default function PostItem({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) {
+  return (
+    <PostItemLink to={slug}>
+      <Container>
+        <PostItemTag background={background}>{category}</PostItemTag>
+        <PostItemInfo>
+          <PostItemDate>
+            {date} - {timeToRead} min de leitura
+          </PostItemDate>
+          <PostItemTitle>{title}</PostItemTitle>
+          <PostItemDescription>{description}</PostItemDescription>
+        </PostItemInfo>
+      </Container>
+    </PostItemLink>
+  );
+}
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+PostItem.defaultProps = {
+  background: '#1fa1f2',
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,21 @@ import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
+import PostItem from '../components/PostItem';
+
 export default function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Home</h1>
+      <PostItem
+        slug="/about"
+        background="red"
+        category="misc"
+        date="01 de janeiro de 2019"
+        timeToRead="5"
+        title="Blog de estudo sobre GatsbyJS"
+        description="Este blog foi desenvolvido com Gatsby, ferramenta que une ReactJS e GraphQL para a criação de sites estáticos (Olá SEO!)"
+      />
     </Layout>
   );
 }
